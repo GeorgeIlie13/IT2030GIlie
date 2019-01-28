38b2e2ed60dc3fec1590cb10761a889481da6afc
George Ilie <S01142968@acad.tri-c.edu>
2019-01-27 22:15:37 -0500

    Lab02

---
+++ WebApplication2/WebApplication2/Controllers/ProductController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: /Product/
+
+        public string Index()
+        {
+            return "Product/Index is displayed";
+        }
+
+        // GET: /Product/Browse/
+
+        public string Browse()
+        {
+            return "Browse displayed";
+        }
+
+        //GET: /Products/Details/105
+
+        public string Details(int id)
+        {
+            string message = "Details displayed for ID =" + id;
+
+            return message;
+        }
+
+        //GET: /Products/Location?zip=44124
+
+        public string Location(int id)
+        {
+            string message = "Location displayed for zip=" + id;
+
+            return message;
+        }
+    }
+}
