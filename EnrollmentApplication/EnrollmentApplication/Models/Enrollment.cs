b75e4e1956867d155f8f30babc7fec7b994f0ccf
George Ilie <S01142968@acad.tri-c.edu>
2019-02-10 21:42:38 -0500

    EnrollmentApplication

---
+++ EnrollmentApplication/EnrollmentApplication/Models/Enrollment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EnrollmentApplication.Models
+{
+    public class Enrollment
+    {
+        public virtual int EnrollmentId { get; set; }
+        public virtual int StudentId { get; set; }
+        public virtual int CourseId { get; set; }
+        public virtual decimal Grade { get; set; }
+        public virtual string StudentObject { get; set; }
+        public virtual string CourseObject { get; set; }
+        public virtual Student Student { get; set; }
+        public virtual Course Course { get; set; }
+        public virtual string EnrollmentArtUrl { get; set; }
+
+    }
+}
